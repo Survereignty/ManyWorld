160d6708722e2bde05c755f23123b023ba0fa68f
Владислав Федотов <survereignty@gmail.com>
2020-04-27 15:39:18 +1100

    public for back

---
+++ Web/mw-backend/public/precache-manifest.b928ed6dff3fdecee70daae17635e9e2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30846d8d0da814f5cfb8",
+    "url": "/css/app.86969e1c.css"
+  },
+  {
+    "revision": "d1c2569577898a7a0e4e",
+    "url": "/css/chunk-50c56c3b.bd656acf.css"
+  },
+  {
+    "revision": "8e22c2a6e3a3c679787e763a97fa11f7",
+    "url": "/fonts/Ubuntu-Medium.8e22c2a6.ttf"
+  },
+  {
+    "revision": "de5edcfb84f870f2fd8124ee0c9f2de4",
+    "url": "/img/angle-arrow-down.de5edcfb.svg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "3181110a5a07d3ded135e96c1f5c1323",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30846d8d0da814f5cfb8",
+    "url": "/js/app.789a0eea.js"
+  },
+  {
+    "revision": "2e71ded5b8ac535d568f",
+    "url": "/js/chunk-2d22d746.8c5198bb.js"
+  },
+  {
+    "revision": "d1c2569577898a7a0e4e",
+    "url": "/js/chunk-50c56c3b.1bbd3591.js"
+  },
+  {
+    "revision": "8e526565dee20e235b49",
+    "url": "/js/chunk-vendors.44e43420.js"
+  },
+  {
+    "revision": "d13420e5efc0defd78e975b86823478d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
