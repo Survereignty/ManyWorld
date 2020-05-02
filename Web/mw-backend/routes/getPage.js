2aa4136a629559f1f04fecebbc5d4ab8a2dba06e
Lirik-Keltran <keltran.987@gmail.com>
2020-05-02 17:00:33 +0300

    Tiken

--- Web/mw-backend/routes/getPage.js
+++ Web/mw-backend/routes/getPage.js
@@ -1,7 +1,5 @@
-const express	= require("express");
-const app		= express();
-const json 		= express.json();
-
+const { Router } = require("express");
+const router = Router(); 
 
 const MongoClient 	= require("mongodb").MongoClient;
 const url 			= "mongodb://localhost:27017/";
@@ -9,24 +7,23 @@ const url 			= "mongodb://localhost:27017/";
 const JWTCreator 	= require("../JWT/jwt");
 const JWT 			= new JWTCreator("R1RLYYVB", "IR1RRLYYVB");
 
-app.get("/", (req, res) => {
+router.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
     res.sendStatus(200);
 });
 
-app.get("/user", json, JWT.VerefyToken.bind(JWT), (req, res) => {
-	const mongoClient = new MongoClient("mongodb://localhost:27017/", { useNewUrlParser: true });
+router.get("/user", JWT.VerefyToken.bind(JWT), (req, res) => {
+
 
     mongoClient.connect((err, client)=>{
 	    const db = client.db("usersdb");
 	    const collection = db.collection("users");
 	    collection.find((err, result) =>{
-	        res.json({
-	        	users: result 
-	        });
+	        console.log(result);
+	        res.sendStatus(200);
 	    });
 	});
 });
 
 
-module.exports = app;
+module.exports = router;
